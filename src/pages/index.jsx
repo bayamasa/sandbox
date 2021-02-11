3a74e1e9cbd39cfe76e4382a0a52471518b30989
hirabayashi-masashi <masashi.hirabayashi@di-v.co.jp>
2021-02-11 13:33:08 +0900

    basic認証を導入

---
+++ src/pages/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Link from "next/link";
+import Basic from "../components/Basic/index"
+
+
+const Index = () =>
+(
+  <div>
+    hello
+  </div>
+)
+;
+
+export default Index;
