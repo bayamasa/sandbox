2438bbd4c128c1823ba7d0037c201c9804a937d8
平林雅士 <masashi@hirabayashimasashinoMacBook-Pro.local>
2021-09-14 23:01:08 +0900

    bubble sort

---
+++ algo/spriral/bubble_sort.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+static const int MAX = 200000;
+
+int main() {
+	int R[MAX], n;
+	cin >> n;
+
+	for(int i = 0; i < n; i++) cin >> R[i];
+
+	for(int i = 0; i < n; i++) 
+	{
+		for (int j = 0; j < n - 1; j++)
+		{
+				if (R[j] > R[j + 1])
+				{
+					int tmp = R[j];
+					R[j] = R[j + 1];
+					R[j + 1] = tmp;
+				}
+		}
+	}
+
+	for (int k=0; k < n; k++)
+	{
+		cout << R[k] << ends;
+	}
+	return 0;
+}
