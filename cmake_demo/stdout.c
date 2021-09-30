8196b93d989ea26fbdf76b344be9e701cd042ccb
bayamasa <mapi0213mgt@gmail.com>
2021-09-30 21:15:54 +0900

    done stdout

---
+++ cmake_demo/stdout.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void p()
+{
+	printf("hello world");
+}
--- cmake_demo/test/gtest.cpp
+++ cmake_demo/test/gtest.cpp
@@ -1,9 +1,16 @@
 #include "gtest/gtest.h"
 
 extern "C" char *print();
+extern "C" void *p();
 
 TEST(cmake_demo, eq_test)
 {
 	ASSERT_STREQ("hello world", print());
 }
 
+TEST(cmake_demo, eq_stdout)
+{
+	testing::internal::CaptureStdout();
+	p();
+	ASSERT_STREQ("hello world", testing::internal::GetCapturedStdout().c_str());
+}
