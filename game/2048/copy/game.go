84aa272457aee16c5b04cad678134525b7edb1dd
bayamasa <mapi0213mgt@gmail.com>
2021-12-07 09:16:56 +0900

    done

---
+++ game/2048/copy/game.go
@@ -0,0 +1,24 @@
+package copy
+
+import (
+	"math/rand"
+	"time"
+
+	"github.com/hajimehoshi/ebiten/v2"
+)
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+const (
+	ScreenWidth  = 420
+	ScreenHeight = 600
+	boardSize    = 4
+)
+
+type Game struct {
+	input *Input
+	board *Board
+	boardImage *ebiten.Image
+}
