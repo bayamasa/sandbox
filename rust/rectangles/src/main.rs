30c1c6eef5c1019da9f444a173815539f0e2ccc1
bayamasa <mapi0213mgt@gmail.com>
2022-02-07 23:44:03 +0900

    メソッド実装

--- rust/rectangles/src/main.rs
+++ rust/rectangles/src/main.rs
@@ -1,4 +1,3 @@
-#[derive(Debug)]
 struct Rectangle {
 	width: u32,
 	height: u32,
@@ -8,13 +7,17 @@ impl Rectangle {
 	fn area(&self) -> u32 {
 		self.width * self.height
 	}
+	fn can_hold(&self, rect: &Rectangle) -> bool {
+		 self.height >= rect.height && self.width >= rect.width
+	}
 }
 
 fn main() {
-	let rect1 = Rectangle {width: 30, height : 50};
-	
-	println!(
-		"The area of the rectangle is {} square pixels.",
-		rect1.area()
-	);
+	let rect1 = Rectangle { width: 30, height: 50 };
+	let rect2 = Rectangle { width: 10, height: 40 };
+	let rect3 = Rectangle { width: 60, height: 45 };
+
+	// rect1にrect2ははまり込む？
+	println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+	println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
 }
